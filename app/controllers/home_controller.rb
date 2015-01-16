c6b740cae5c9f5c6eedcae6e465a0e29d1e56977
Jef <jef.own22@gmail.com>
2015-01-16 03:42:29 -0300

    Lógica para exibição de destaques na home feita.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @destaque = Imovel.where("destaque = ?", true).limit(8)
+    @destaques = Imovel.where("destaque = ?", true).limit(8)
   end
 end
