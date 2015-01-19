fb9c6b4ca315d575898746f5150ac24369597353
Jef <jef.own22@gmail.com>
2015-01-19 15:04:06 -0300

    Instalado materialize com bower.

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
 module SuperSite
   class Application < Rails::Application
