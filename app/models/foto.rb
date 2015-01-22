2aa8561456d3b3ea8f1c17b20b7b9a49e5299972
Jef <jef.own22@gmail.com>
2015-01-22 18:55:52 -0300

    Thumbnails imovel feito.

--- app/models/foto.rb
+++ app/models/foto.rb
@@ -1,7 +1,7 @@
 class Foto < ActiveRecord::Base
   belongs_to :imovel
 
-  has_attached_file :image, :styles => { :thumb => "200x200#", :medium => "1280x960" }
+  has_attached_file :image, :styles => { :thumb => "270x200#", :medium => "1280x960" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 
 end
