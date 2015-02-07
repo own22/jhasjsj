cd3f81bbf2b9826c8e63f2842444085d15a31b0e
Jef <jef.own22@gmail.com>
2015-02-07 16:29:54 -0300

    Fixando assets 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( imovels.js )
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
