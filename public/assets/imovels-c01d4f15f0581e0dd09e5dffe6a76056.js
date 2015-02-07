9a0a0f21028340ce7f95d530dd2dd3808bd7748b
Jef <jef.own22@gmail.com>
2015-02-06 22:39:05 -0300

    Precompiling

---
+++ public/assets/imovels-c01d4f15f0581e0dd09e5dffe6a76056.js
@@ -0,0 +1,245 @@
+/*
+
+(function() {
+  jQuery(function() {
+    var bairros;
+    var cidadeDomId = ($("#new_imovel").length) ? "#imovel_cidade_id":"#cidade_id"
+    var bairroDomId = ($("#new_imovel").length) ? "#imovel_bairro_id":"#bairro_id"
+    console.log(cidadeDomId)
+    console.log(bairroDomId)
+
+    bairros = $(bairroDomId).html();
+    $(cidadeDomId).change(function() {
+      var cidade, options;
+      cidade = $(cidadeDomId + ' :selected').text();
+      options = $(bairros).filter("optgroup[label='" + cidade + "']").html();
+      if (options) {
+        options = "<option value=''> Selecione Bairro </option>" + options;
+        return $(bairroDomId).html(options);
+      } else if (cidade === "Escolha a cidade") {
+        return $(bairroDomId).html(bairros);
+      } else {
+        return $(bairroDomId).empty();
+      }
+    });
+
+    $(bairroDomId).change(function() {
+      var bairro, selecionar;
+      cidade = $(this.options[this.selectedIndex]).closest('optgroup').prop('label');
+      $(cidadeDomId + ' option').filter(function() {
+        return ($(this).text() == cidade);
+      }).prop('selected', true);
+    });
+  });
+}).call(this);
+
+$(document).ready(function() {
+  $('#slider-range').slider({
+    range: true,
+    min: 10000,
+    max: 1000000,
+    values: [50000, 700000],
+    slide: function(event, ui) {
+      $('#preco_min').val(ui.values[0]);
+      $('#preco_max').val(ui.values[1]);  
+
+    }  
+  });
+  $("#amount").val("R$" + $("#slider-range").slider("values", 0) + " - R$" + $("#slider-range").slider("values", 1));
+});
+*/
+
+
+$(document).ready(function(){
+  var width = $(window).width()
+  // $('.slider').slider({full_width: true, height: 250});
+
+  $("#floatContato").sticky({topSpacing:300});
+
+  if (width <= 600) {    
+    $(".showImovel").removeClass("card-panel");
+    $(".row").removeClass("row");
+    $(".body.container").removeClass("container");
+    $('.slider').slider({full_width: true, height: 250});
+  } else {
+    $('.slider').slider({full_width: true, height: 500});
+  }
+
+  if ( width >= 601 && width <= 992) {
+    $(".retangle").addClass("card-panel");
+  }
+
+}); 
+
+// Sticky Plugin v1.0.0 for jQuery
+// =============
+// Author: Anthony Garand
+// Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
+// Improvements by Leonardo C. Daronco (daronco)
+// Created: 2/14/2011
+// Date: 2/12/2012
+// Website: http://labs.anthonygarand.com/sticky
+// Description: Makes an element on the page stick on the screen as you scroll
+//       It will only set the 'top' and 'position' of your element, you
+//       might need to adjust the width in some cases.
+
+(function($) {
+  var defaults = {
+      topSpacing: 0,
+      bottomSpacing: 0,
+      className: 'is-sticky',
+      wrapperClassName: 'sticky-wrapper',
+      center: false,
+      getWidthFrom: '',
+      responsiveWidth: false
+    },
+    $window = $(window),
+    $document = $(document),
+    sticked = [],
+    windowHeight = $window.height(),
+    scroller = function() {
+      var scrollTop = $window.scrollTop(),
+        documentHeight = $document.height(),
+        dwh = documentHeight - windowHeight,
+        extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
+
+      for (var i = 0; i < sticked.length; i++) {
+        var s = sticked[i],
+          elementTop = s.stickyWrapper.offset().top,
+          etse = elementTop - s.topSpacing - extra;
+
+        if (scrollTop <= etse) {
+          if (s.currentTop !== null) {
+            s.stickyElement
+              .css('width', '')
+              .css('position', '')
+              .css('top', '');
+            s.stickyElement.trigger('sticky-end', [s]).parent().removeClass(s.className);
+            s.currentTop = null;
+          }
+        }
+        else {
+          var newTop = documentHeight - s.stickyElement.outerHeight()
+            - s.topSpacing - s.bottomSpacing - scrollTop - extra;
+          if (newTop < 0) {
+            newTop = newTop + s.topSpacing;
+          } else {
+            newTop = s.topSpacing;
+          }
+          if (s.currentTop != newTop) {
+            s.stickyElement
+              .css('width', s.stickyElement.width())
+              .css('position', 'fixed')
+              .css('top', newTop);
+
+            if (typeof s.getWidthFrom !== 'undefined') {
+              s.stickyElement.css('width', $(s.getWidthFrom).width());
+            }
+
+            s.stickyElement.trigger('sticky-start', [s]).parent().addClass(s.className);
+            s.currentTop = newTop;
+          }
+        }
+      }
+    },
+    resizer = function() {
+      windowHeight = $window.height();
+
+      for (var i = 0; i < sticked.length; i++) {
+        var s = sticked[i];
+        if (typeof s.getWidthFrom !== 'undefined' && s.responsiveWidth === true) {
+          s.stickyElement.css('width', $(s.getWidthFrom).width());
+        }
+      }
+    },
+    methods = {
+      init: function(options) {
+        var o = $.extend({}, defaults, options);
+        return this.each(function() {
+          var stickyElement = $(this);
+
+          var stickyId = stickyElement.attr('id');
+          var wrapperId = stickyId ? stickyId + '-' + defaults.wrapperClassName : defaults.wrapperClassName 
+          var wrapper = $('<div></div>')
+            .attr('id', stickyId + '-sticky-wrapper')
+            .addClass(o.wrapperClassName);
+          stickyElement.wrapAll(wrapper);
+
+          if (o.center) {
+            stickyElement.parent().css({width:stickyElement.outerWidth(),marginLeft:"auto",marginRight:"auto"});
+          }
+
+          if (stickyElement.css("float") == "right") {
+            stickyElement.css({"float":"none"}).parent().css({"float":"right"});
+          }
+
+          var stickyWrapper = stickyElement.parent();
+          stickyWrapper.css('height', stickyElement.outerHeight());
+          sticked.push({
+            topSpacing: o.topSpacing,
+            bottomSpacing: o.bottomSpacing,
+            stickyElement: stickyElement,
+            currentTop: null,
+            stickyWrapper: stickyWrapper,
+            className: o.className,
+            getWidthFrom: o.getWidthFrom,
+            responsiveWidth: o.responsiveWidth
+          });
+        });
+      },
+      update: scroller,
+      unstick: function(options) {
+        return this.each(function() {
+          var unstickyElement = $(this);
+
+          var removeIdx = -1;
+          for (var i = 0; i < sticked.length; i++)
+          {
+            if (sticked[i].stickyElement.get(0) == unstickyElement.get(0))
+            {
+                removeIdx = i;
+            }
+          }
+          if(removeIdx != -1)
+          {
+            sticked.splice(removeIdx,1);
+            unstickyElement.unwrap();
+            unstickyElement.removeAttr('style');
+          }
+        });
+      }
+    };
+
+  // should be more efficient than using $window.scroll(scroller) and $window.resize(resizer):
+  if (window.addEventListener) {
+    window.addEventListener('scroll', scroller, false);
+    window.addEventListener('resize', resizer, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onscroll', scroller);
+    window.attachEvent('onresize', resizer);
+  }
+
+  $.fn.sticky = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
+  };
+
+  $.fn.unstick = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method ) {
+      return methods.unstick.apply( this, arguments );
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
+
+  };
+  $(function() {
+    setTimeout(scroller, 0);
+  });
+})(jQuery);
