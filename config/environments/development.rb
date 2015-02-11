f89011cfe07457666e4833ed32375efea78efcc3
Jef <jef.own22@gmail.com>
2015-02-11 13:22:39 -0300

    Aplicando sugestões do facebook

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,13 +36,13 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
    # Amazon S3 settings for Paperclip uploads
-    config.paperclip_defaults = {
-      :storage => :s3,
-      # :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
-      :s3_credentials => {
-        :bucket => ENV['S3_BUCKET_NAME'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
+    # config.paperclip_defaults = {
+    #   :storage => :s3,
+    #   # :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
+    #   :s3_credentials => {
+    #     :bucket => ENV['S3_BUCKET_NAME'],
+    #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    #   }
+    # }
 end
